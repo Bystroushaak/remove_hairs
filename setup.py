431b19a85f5e776dada4e7978f0c513cbfaefcdf
Bystroushaak <bystrousak@kitakitsune.org>
2015-03-24 12:18:06 +0100

    setup.py: Added functions from removed docs/.

--- setup.py
+++ setup.py
@@ -6,15 +6,30 @@
 # Imports =====================================================================
 from distutils.core import setup
 
-from docs import getVersion
-
-
 # Variables ===================================================================
 changelog = open('CHANGES.rst').read()
 long_description = "\n\n".join([
     open('README.rst').read(),
     changelog
 ])
+
+
+# Functions & classes =========================================================
+def allSame(s):
+    return not filter(lambda x: x != s[0], s)
+
+
+def hasDigit(s):
+    return any(map(lambda x: x.isdigit(), s))
+
+
+def getVersion(data):
+    data = data.splitlines()
+    return filter(
+        lambda (x, y):
+            len(x) == len(y) and allSame(y) and hasDigit(x) and "." in x,
+        zip(data, data[1:])
+    )[0][0]
 
 
 # Actual setup definition =====================================================
