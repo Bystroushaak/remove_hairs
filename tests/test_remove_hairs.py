bfec71ddc026c82f590256b26e57685573dab22f
Bystroushaak <bystrousak@kitakitsune.org>
2015-03-24 12:02:34 +0100

    Added tests of the code.

--- tests/test_remove_hairs.py
+++ tests/test_remove_hairs.py
@@ -6,17 +6,49 @@
 # Imports =====================================================================
 import sys
 
-
 import pytest
 
 sys.path.append('src/')
-import remove_hairs
-
-
-# Variables ===================================================================
-
+from remove_hairs import remove_hairs
+from remove_hairs import remove_hairs_decorator
 
 
 # Functions & classes =========================================================
 def test_remove_hairs():
-    pass
+    assert remove_hairs(",a-sd,-/") == "a-sd"
+
+    assert remove_hairs(" - a sd. --", " -") == "a sd."
+
+    assert remove_hairs(" -  --", " -") == ""
+
+    assert remove_hairs(" -  --", "") == " -  --"
+
+    assert remove_hairs("", "") == ""
+
+    assert remove_hairs("", "-,.") == ""
+
+
+def test_remove_hairs_decorator():
+    @remove_hairs_decorator
+    def x():
+        return ",a-sd,-/"
+
+    assert x() == "a-sd"
+
+    @remove_hairs_decorator
+    def x(a):
+        return ",a-sd,-/"
+
+    assert x(1) == "a-sd"
+
+    @remove_hairs_decorator(hairs=" -")
+    def y():
+        return " - a sd. --"
+
+    assert y() == "a sd."
+
+    @remove_hairs_decorator(hairs=" -")
+    def y(a):
+        return " - a sd. --"
+
+    assert y(1) == "a sd."
